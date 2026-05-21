a995284c6db6b01a4012e1d269fa663930fce8c4
Erick Fernando Carvalho Sanchez <carvalhosanchezerickfernando@gmail.com>
2026-05-20 23:27:24 -0300

    http codes y mensajes como constantes

--- app_backend/routes/ping.py
+++ app_backend/routes/ping.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, jsonify
+from http_codes_and_messages import HTTP_OK
 
-ping_bp = Blueprint('ping', __name__)
+ping_bp = Blueprint("ping", __name__)
 
-@ping_bp.route('/ping', methods=['GET'])
+
+@ping_bp.route("/ping", methods=["GET"])
 def ping():
-    return jsonify({'message': 'pong'})
+    return jsonify({"message": "pong"}), HTTP_OK
